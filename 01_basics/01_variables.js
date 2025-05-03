c518e860d35044e3c415a5f7f41b7bccb5e5bb2a
jt31981 <jevin.talaviya@stud.th-deg.de>
2025-05-03 09:10:30 +0200

    Variable in Javascript

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 2566995
 let accountEmail = "jevintalaviya@gmail.com"
 var accountPass = "Learning"
 userCity = "Surat"
+let accountState;   
 
 // accountId = 255455   // const is immutable as string in  python.//
 accountEmail = "jev.talaviya@yahoo.gmail.com"
@@ -9,4 +10,8 @@ accountPass = "Jw-evin"
 userCity = "Rajkot"
 console.log(accountId);
 
-console.table([accountEmail, accountPass, userCity])
+console.table([accountEmail, accountPass, userCity, accountState])
+/*
+Prefer not to use var in javascript, Because 
+of issues in block scope and functiona; scope
+*/
