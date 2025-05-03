0977f2917f8c432c0780c49fa2661f290ea26312
jt31981 <jevin.talaviya@stud.th-deg.de>
2025-05-03 08:44:39 +0200

    variables in Javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 2566995
+let accountEmail = "jevintalaviya@gmail.com"
+var accountPass = "Learning"
+userCity = "Surat"
+
+// accountId = 255455   // const is immutable as string in  python.//
+accountEmail = "jev.talaviya@yahoo.gmail.com"
+accountPass = "Jw-evin"
+userCity = "Rajkot"
+console.log(accountId);
+
+console.table([accountEmail, accountPass, userCity])
