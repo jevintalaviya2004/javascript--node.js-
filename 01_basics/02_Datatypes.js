838b46c9dd7fea22902f45233184fbcccf02e1ff
jt31981 <jevin.talaviya@stud.th-deg.de>
2025-05-06 19:05:57 +0200

    Datatypes in  Javascript

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all JS Code as ewer version
+
+//alert( 3 + 3)   // We are using Node.js, not browser.
+
+let name = "Jevin" //String datatype
+let age = 22  //int or number datatype
+let userloggedIN = true // Boolean Datatype
+
+
+// number with range => 2 to power 53.
+// bigint for numer greater than 2^53
+//string     => ""
+// boolean => true/false
+//null => standalone Value
+//Undefined => when the value is not assigned
+// symbol => to define uniqueness
+
+
+//non-primitive 
+//object  **(typeof null)**
+
+console.log(typeof "Jevin"); //to know the type "typeof"
+
+
