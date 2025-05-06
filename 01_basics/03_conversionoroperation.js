662e3d386a46a2da703b6699c15c7746020f4109
jt31981 <jevin.talaviya@stud.th-deg.de>
2025-05-06 19:42:16 +0200

    Coversions And Operations in Javascript

---
+++ 01_basics/03_conversionoroperation.js
@@ -0,0 +1,32 @@
+let age = "33abc"
+
+console.log(typeof age);
+console.log(typeof (age));
+
+let ValueInNumber = Number(age);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+//"33" => 33
+//"33xyz" => NaN
+//true=> 1, false=> 0
+
+let LoggedIn = 1
+
+let booleanloggedIn = Boolean(LoggedIn)
+console.log(typeof booleanloggedIn);
+console.log(booleanloggedIn);
+
+//1 => true
+//0 => false
+//""(Empty value) => false
+//"jevin"(with some value) => true
+
+let somenumber = 33;
+
+let Stringnumber = String(somenumber);
+console.log(Stringnumber);
+console.log(typeof Stringnumber);
+
+
+
